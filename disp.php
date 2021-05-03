83c89c80bf5bce7568a7c100c28d445bbec9fad9
dioh0922 <diohound@gmail.com>
2021-05-03 10:07:43 +0900

    init

---
+++ disp.php
@@ -0,0 +1,16 @@
+<?php
+$date = date();
+
+?>
+<!DOCTYPE html>
+<html lang="ja" dir="ltr">
+	<head>
+		<meta charset="utf-8">
+		<title>動確</title>
+	</head>
+	<body>
+		<?php echo $date; ?>
+		<br>
+		確認ページ
+	</body>
+</html>
